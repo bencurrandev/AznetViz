af5ff65f46f5731b1bb820730b2e473ab7467e9a
Ben Curran <benjamin.curran@fmgl.com.au>
2021-01-21 14:47:49 +0800

    Added ExpressRoute and VNG

--- azure/AzNetwork/vnettomd2.ps1
+++ azure/AzNetwork/vnettomd2.ps1
@@ -1,32 +1,94 @@
-$sharednet = Get-AzVirtualNetwork
 $subscriptions = Get-AzSubscription
-foreach ($vnet in $sharednet) {
-    if ($vnet.DhcpOptions) {
-        $vnetName = $vnet.Name -Replace '-(?:[0-9]{1,3}\.){3}[0-9]{1,3}', ''
-    } else { 
-        $vnetName = $vnet.Name -Replace '-(?:[0-9]{1,3}\.){3}[0-9]{1,3}', '-Citrix'
+$currentCon = Get-AzContext
+$subErc = @()
+$subVngs = @()
+$subVngConns = @()
+foreach ($sub in $subscriptions) {
+    Select-AzSubscription $sub.Id | Out-Null
+    $thisSubVnets = Get-AzVirtualNetwork
+    $subVnets += $thisSubVnets
+    foreach ($vnet in $thisSubVnets) {
+        $subVngList = ($vnet.Subnets | where Name -eq 'GatewaySubnet').Ipconfigurations.Id
+        foreach ($vng in $subVngList) {
+            $thisSubVngs = Get-AzVirtualNetworkGateway -ResourceGroupName $vng.Split("/")[4] -Name $vng.Split("/")[8]
+            $subVngs += $thisSubVngs
+            $vng1text = "`"$($vng.Split("/ipCon")[0])`""
+            $subVngConns += Get-AzVirtualNetworkGatewayConnection -ResourceGroupName $vng.Split("/")[4] | where VirtualNetworkGateway1Text -eq $vng1text
+        }
     }
-    write-host "$($vnetName) {"
-    write-host "resourcegroup $($vnet.ResourceGroupName.Replace(".","-"))"
-    write-host "subscription $(($subscriptions | where Id -eq $sharednet[0].Id.Substring(15,36)).Name.Replace(" ","-"))"
-    write-host "}"
+    
+    $subErc += Get-AzExpressRouteCircuit -WarningAction silentlyContinue
+    # $subVwan += Get-AzVirtualWan
+    # foreach
+}
+Select-AzSubscription $currentCon.Subscription | Out-Null
+$output = @()
+$vnetList = @()
+$output += "erDiagram"
+foreach ($vnet in $subVnets) {
+    # write-host "Processing $($vnet.Name) in $(($subscriptions | where Id -eq $vnet.Id.Substring(15,36)).Name.Replace(" ","-"))"
+    if ($vnet.DhcpOptions) {
+        $vnetName = $vnet.Name -Replace '-(?:[0-9]{1,3}\.){3,4}[0-9]{1,3}', ''
+    } else { 
+        $vnetName = $vnet.Name -Replace '-(?:[0-9]{1,3}\.){3,4}[0-9]{1,3}', '-OffNet'
+    }
+    $vnetList += $vnetName
+    $output += "    $($vnetName) {"
+    $output += "        resourcegroup $($vnet.ResourceGroupName.Replace(".","-"))"
+    $output += "        subscription $(($subscriptions | where Id -eq $vnet.Id.Substring(15,36)).Name.Replace(" ","-").Replace("/","-"))"
+    $output += "        location $($vnet.location)"
+    $output += "        type $($vnet.Type.Replace("/","-").Replace(".","-"))"
+    $output += "    }"
     foreach ($peering in $vnet.VirtualNetworkPeerings) {
-        $peervNet = $sharednet | where Id -eq $peering.RemoteVirtualNetwork.Id
+        $peervNet = $subVnets | where Id -eq $peering.RemoteVirtualNetwork.Id
         if ($peervNet) {
             if ($peervNet.DhcpOptions) {
-                $peerName = $peervNet.Name -Replace '-(?:[0-9]{1,3}\.){3}[0-9]{1,3}', ''
+                $peerName = $peervNet.Name -Replace '-(?:[0-9]{1,3}\.){3,4}[0-9]{1,3}', ''
             } else { 
-                $peerName = $peervNet.Name -Replace '-(?:[0-9]{1,3}\.){3}[0-9]{1,3}', '-Citrix'
+                $peerName = $peervNet.Name -Replace '-(?:[0-9]{1,3}\.){3,4}[0-9]{1,3}', '-OffNet'
             }
         } else {
-            $peerName = (($peering.RemoteVirtualNetwork.Id).Split("/"))[8] -Replace '-(?:[0-9]{1,3}\.){3}[0-9]{1,3}', ''
+            $peerName = (($peering.RemoteVirtualNetwork.Id).Split("/"))[8] -Replace '-(?:[0-9]{1,3}\.){3,4}[0-9]{1,3}', ''
         }
-        if ($peering.AllowGatewayTransit -eq $true) {
-            write-host "$($vnetName) }|--|| $($peerName) : peering"
-        } elseif ($peering.UseRemoteGateways -eq $true) {
-            write-host "$($vnetName) ||--|{ $($peerName) : peering"
+        if ($vnetList -contains $peerName) {
+
         } else {
-            write-host "$($vnetName) |o--o| $($peerName) : peering"
+            if ($peering.AllowGatewayTransit -eq $true) {
+                $output += "    $($vnetName) }|--|| $($peerName) : peering"
+            } elseif ($peering.UseRemoteGateways -eq $true) {
+                $output += "    $($vnetName) ||--|{ $($peerName) : peering"
+            } else {
+                $output += "    $($vnetName) |o--o| $($peerName) : peering"
+            }
         }
     }
 }
+foreach ($erc in $subErc) {
+    $output += "    $($erc.name) {"
+    $output += "        resourcegroup $($erc.ResourceGroupName.Replace(".","-"))"
+    $output += "        subscription $(($subscriptions | where Id -eq $erc.Id.Substring(15,36)).Name.Replace(" ","-").Replace("/","-"))"
+    $output += "        location $($erc.location)"
+    $output += "        type $($erc.Type.Replace("/","-").Replace(".","-"))"
+    $output += "    }"
+}
+foreach ($vng in $subVngs) {
+    $output += "    $($vng.name) {"
+    $output += "        resourcegroup $($vng.ResourceGroupName.Replace(".","-"))"
+    $output += "        subscription $(($subscriptions | where Id -eq $vng.Id.Substring(15,36)).Name.Replace(" ","-").Replace("/","-"))"
+    $output += "        location $($vng.location)"
+    $output += "        type $($vng.Type.Replace("/","-").Replace(".","-"))-$($vng.GatewayType)"
+    $output += "    }"
+    $vngVnetId = ($vng.IpConfigurations.subnet | where id -like "*GatewaySubnet*").Id
+    $vngVnet = ($vngVnetId.Split("/"))[8] -Replace '-(?:[0-9]{1,3}\.){3,4}[0-9]{1,3}', ''
+    $output += "    $($vng.name) ||--|| $($vngVnet) : gateway"
+}
+foreach ($vngConn in $subVngConns) {
+    if ($vngConn.VirtualNetworkGateway2) {
+        $output += "    $($vngConn.VirtualNetworkGateway1.Id.Split("/")[8]) ||--|| $($vngConn.VirtualNetworkGateway2.Id.Split("/")[8]) : connection"
+    } elseif ($vngConn.LocalNetworkGateway2) {
+        $output += "    $($vngConn.VirtualNetworkGateway1.Id.Split("/")[8]) ||--|| $($vngConn.LocalNetworkGateway2.Id.Split("/")[8]) : connection"
+    } elseif ($vngConn.Peer) {
+        $output += "    $($vngConn.VirtualNetworkGateway1.Id.Split("/")[8]) ||--|| $($vngConn.Peer.Id.Split("/")[8]) : connection"
+    }
+}
+$output
